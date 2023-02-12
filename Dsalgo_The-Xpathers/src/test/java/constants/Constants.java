cb8fc2a61b28138bc3fbbe81f717f7e1560c8894
Monika morawadkar <marihalm47@gmail.com>
2023-02-11 19:39:33 -0600

    Add files via upload

---
+++ Dsalgo_The-Xpathers/src/test/java/constants/Constants.java
@@ -0,0 +1,24 @@
+package constants;
+
+public class Constants {
+	
+	public static String APP_URL;
+	public static String BROWSER;
+	public static String USERNAME;
+	public static String PASSWORD;
+	public static String REG_URL;
+	public static String SIGNIN_URL;
+	public static String HOME_URL;
+	public static String EXCEL_PATH;
+	
+	//Queue page 
+		public static String HOMEPAGEURL;
+		public static String EDITORURL;
+		public static String QUEUEPAGEURL;
+		public static String QUEUEOPRATIONLINK;
+		
+		//graph page
+		public static String GRAPHPAGEURL;
+		public static String GRAPHEDITORURL;
+		public static String GRAPHOPRATIONLINK;
+}
---
+++ Dsalgo_The-Xpathers/src/test/java/utilities/common_utils.java
@@ -0,0 +1,111 @@
+package utilities;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Properties;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.support.PageFactory;
+
+import constants.Constants;
+import page_objects.ArrayPage;
+import page_objects.GraphPage;
+import page_objects.HomePage;
+import page_objects.QueuePage;
+import page_objects.Registerpage;
+import page_objects.SigninPage;
+import webdriver_manager.DriverManager;
+
+public class common_utils {
+	
+	public void loadproperties() {
+				
+		Properties properties = new Properties();
+		try {
+			properties.load(getClass().getResourceAsStream("/config.properties"));
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+		Constants.APP_URL = properties.getProperty("APP_URL");
+		Constants.BROWSER = properties.getProperty("BROWSER");
+		Constants.USERNAME= properties.getProperty("USERNAME");
+		Constants.PASSWORD = properties.getProperty("PASSWORD");
+		Constants.REG_URL= properties.getProperty("REG_URL");
+		Constants.SIGNIN_URL=properties.getProperty("SIGNIN_URL");
+		Constants.HOME_URL=properties.getProperty("HOME_URL");
+		Constants.EXCEL_PATH=properties.getProperty("EXCEL_PATH");
+		
+		//queue page
+				Constants.SIGNIN_URL = properties.getProperty("SIGNIN_URL");
+				Constants.HOMEPAGEURL = properties.getProperty("HOMEPAGEURL");
+				Constants.EDITORURL = properties.getProperty("EDITORURL");
+				Constants.QUEUEPAGEURL = properties.getProperty("QUEUEPAGEURL");
+				Constants.QUEUEOPRATIONLINK = properties.getProperty("QUEUEOPRATIONLINK");
+				
+				//graph page
+				Constants.GRAPHPAGEURL = properties.getProperty("GRAPHPAGEURL");
+				Constants.GRAPHEDITORURL = properties.getProperty("GRAPHEDITORURL");
+				Constants.GRAPHOPRATIONLINK = properties.getProperty("GRAPHOPRATIONLINK");
+		
+	}
+	
+	//All the page class has to be initialized in this method
+	public void initWebElements()
+	{
+		PageFactory.initElements(DriverManager.getDriver(), HomePage.getInstance());
+		PageFactory.initElements(DriverManager.getDriver(), Registerpage.getInstance());
+		PageFactory.initElements(DriverManager.getDriver(), SigninPage.getInstance());
+		PageFactory.initElements(DriverManager.getDriver(), ArrayPage.getInstance());
+		PageFactory.initElements(DriverManager.getDriver(), QueuePage.getInstance());
+		PageFactory.initElements(DriverManager.getDriver(), GraphPage.getInstance());
+	}
+	
+	public ArrayList<ArrayList<String>> readExcelData()
+	{
+		ArrayList<ArrayList<String>> arrLL = new ArrayList<ArrayList<String>>();
+		   
+		File f = new File(this.getClass().getResource("/TestCaseData.xlsx").getFile());
+		System.out.println(f.getAbsoluteFile());
+		
+		FileInputStream fis;
+		XSSFWorkbook excelWorkbook;
+		XSSFSheet excelSheet;
+		try {
+			fis = new FileInputStream(f);		
+			excelWorkbook = new XSSFWorkbook(fis);
+			excelSheet = excelWorkbook.getSheetAt(0);
+			int rows = excelSheet.getPhysicalNumberOfRows();
+			int cols = excelSheet.getRow(0).getPhysicalNumberOfCells();
+			String data[][]= new String[rows][cols];
+			XSSFCell cell;
+			for(int i =0;i<rows;i++)
+			{
+				arrLL.add(new ArrayList<String>());
+				for(int j=0;j<cols;j++)
+				{
+					cell = excelSheet.getRow(i).getCell(j);
+					String cellContents=cell.getStringCellValue();
+					data[i][j] = cellContents;
+					//System.out.print(" " + data[i][j]);
+					 arrLL.get(i).add(j, cellContents);
+				}
+				//System.out.println();
+			}			
+	        //System.out.println(arrLL);
+			fis.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return arrLL;
+	}
+		
+
+}
